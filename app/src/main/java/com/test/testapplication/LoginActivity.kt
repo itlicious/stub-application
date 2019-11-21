b93dd1f980c4205f02f65b1cff30f52ab64ed128
jovandereyt@gmail.com <jovandereyt@gmail.com>
2019-11-21 12:47:34 +0100

    init

---
+++ app/src/main/java/com/test/testapplication/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.test.testapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/test/testapplication/OverviewActivity.kt
@@ -0,0 +1,13 @@
+package com.test.testapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class OverviewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_overview)
+    }
+
+}
